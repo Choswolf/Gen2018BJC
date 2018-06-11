9c111a35cd226781ce6259b5347b59086fe958d8
Choswolf <julvelazquez@hotmail.com>
2018-06-11 17:45:52 -0500

    4ta prueba

--- Gengit/prueba programa/src/com/appholamunditu/main/Holamuntitu.java
+++ Gengit/prueba programa/src/com/appholamunditu/main/Holamuntitu.java
@@ -5,6 +5,7 @@ public class Holamuntitu {
 	public static void main(String[] args) {
 		System.out.println("Hola Munditu");
 		System.out.println("Buenos dias Julio!");
+		System.out.println("BUEN SABADO");
 
 	}
 
